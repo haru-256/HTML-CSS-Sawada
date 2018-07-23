70d1e384f85d0a6f9cb83a439bdc6048802ea2dc
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-07-23 16:40:17 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <title>犬とご飯</title>
+        <meta name="description" content="犬と一緒に美味しいご飯を食べて生きています">
+        <style>
+            p {
+                color:green;
+                font-size: 14px;
+            }
+            p {
+                color:red;
+            }
+        </style>
+        <!-- <link rel="stylesheet" href="main.css"> -->
+    </head>
+    <body>
+        <h1>みんな大好きハンバーグ</h1>
+        <p>美味しいハンバーグを作りなさい<br>
+            今日は牛肉と豚肉の合挽肉を使用す．</p>
+        <?php
+            echo "HELLO WORLO";
+        ?>
+    </body>
+</html>
